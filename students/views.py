26ee51bedec74e25dca6e50a926b09b664af27f1
Andrii Pertsov <andrew.percov@gmail.com>
2016-07-18 18:22:43 +0300

    dynamic table

--- students/views.py
+++ students/views.py
@@ -1,21 +1,51 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Views for Students
+
+
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+    {'id': 1,
+    'first_name': u'Корост',
+    'last_name': u'Андрій',
+    'ticket': 2123,
+    'image': 'img/1.jpeg'},
+    {'id': 2,
+    'first_name': u'Иваник',
+    'last_name': u'Андрій',
+    'ticket': 1111,
+    'image': 'img/2.jpeg'},
+    {'id': 3,
+    'first_name': u'Ляшко',
+    'last_name': u'Петро',
+    'ticket': 1541,
+    'image': 'img/3.jpeg'}
+    )
+    return render(request, 'students/students_list.html', {'students': students})
+
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
+
 def students_edit(request, sid):
     return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
 
 # Views for Groups
 def groups_list(request):
     return HttpResponse('<h1>Groups Listing</h1>')
+
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
+
 def groups_edit(request, gid):
     return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
 def groups_delete(request, gid):
     return HttpResponse('<h1>Delete Group %s</h1>' % gid)
