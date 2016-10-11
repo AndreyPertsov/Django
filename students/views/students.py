af1145a6a0eb73f73f79ea9ccd90d98dd5b06b80
Andrii Pertsov <andrew.percov@gmail.com>
2016-10-11 21:09:30 +0300

    students_add view+alerts

--- students/views/students.py
+++ students/views/students.py
@@ -6,6 +6,8 @@ from django.core.urlresolvers import reverse
 from ..models.student import Student
 from ..models.group import Group
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from datetime import datetime
+from PIL import Image
 
 # Views for Students
 
@@ -45,36 +47,94 @@ def students_add(request):
         if request.POST.get('add_button') is not None:
 
             # TODO: validate input from user
+            # errors collection
             errors = {}
 
-            # create student object
-            if not errors:
-                student = Student(
-                    first_name=request.POST['first_name'],
-                    last_name=request.POST['last_name'],
-                    middle_name=request.POST['middle_name'],
-                    birthday=request.POST['birthday'],
-                    ticket=request.POST['ticket'],
-                    student_group=Group.objects.get(pk=request.POST['student_group']),
-                    photo=request.FILES['photo'],
-                )
-                # save it to database
-                student.save()
+            # validate student data will go here
+            data = {'middle_name': request.POST.get('middle_name'), 'notes': request.POST.get('notes')}
 
-                # Повертаємо користувача до списку студентів
-                # redirect user to students list
-                return HttpResponseRedirect(reverse('home'))
+            # validate user input
+            first_name = request.POST.get('first_name', '').strip()
+            if not first_name:
+                errors['first_name'] = u"Ім'я є обов'язковим"
+            else:
+                data['first_name'] = first_name
+
+            last_name = request.POST.get('last_name', '').strip()
+            if not last_name:
+                errors['last_name'] = u"Прізвище є обов'язковим"
+            else:
+                data['last_name'] = last_name
+
+            birthday = request.POST.get('birthday', '').strip()
+            if not birthday:
+                errors['birthday'] = u"Дата народження є обов'язковою"
+            else:
+                try:
+                    datetime.strptime(birthday, '%Y-%m-%d')
+                except Exception:
+                    errors['birthday'] = u"Введіть коректний формат дати (напр. 1984-12-30)"
+                else:
+                    data['birthday'] = birthday
+
+            ticket = request.POST.get('ticket', '').strip()
+            if not ticket:
+                errors['ticket'] = u"Номер білета є обов'язковим"
+            else:
+                data['ticket'] = ticket
+
+            student_group = request.POST.get('student_group', '').strip()
+            if not student_group:
+                errors['student_group'] = u"Оберіть групу для студента"
+            else:
+                groups = Group.objects.filter(pk=student_group)
+                if len(groups) != 1:
+                    errors['student_group'] = u"Оберіть коректну групу"
+                else:
+                    data['student_group'] = groups[0]
+
+            photo = request.FILES.get('photo')
+            if not photo:
+                errors['photo'] = u'Додайте фото'
+            if photo:
+                mime = ['image/jpeg', 'image/pjpeg', 'image/png']
+                if photo.content_type not in mime:
+                    errors['photo'] = u'Невірний тип файлу'
+                if photo.size > 2000000:
+                    errors['photo'] = u'Розмір файлу не може бути більшим за 2 мб'
+
+                original = Image.open(photo)
+                s = original.size
+                max_width = 512
+                max_height = 512
+
+                if s > (max_height, max_width):
+                    errors['photo'] = u'Максимальній розмір зображення %s x %s пікселів.' % (max_height, max_width)
+                else:
+                    data['photo'] = photo
+
+            # save student
+            if not errors:
+                student = Student(**data)
+                student.save()
+                # redirect to students list
+                return HttpResponseRedirect(u'%s?status_message=Студента {0} {1} успішно додано!'.format
+                                            (student.first_name, student.last_name) % reverse('home'))
 
             else:
                 # render form with errors and previous user input
-                return render(request, 'students/students_add.html', {'groups': Group.objects.all().order_by('title'), 'errors': errors})
+                return render(request, 'students/students_add.html',
+                              {'groups': Group.objects.all().order_by('title'),
+                               'errors': errors})
 
         elif request.POST.get('cancel_button') is not None:
-           # redirect to home page on cancel button
-           return HttpResponseRedirect(reverse('home'))
+            # redirect to home page on cancel button
+            return HttpResponseRedirect(u'%s?status_message=Додавання студента скасовано!'
+                                        % reverse('home'))
     else:
         # initial form render
-        return render(request, 'students/students_add.html', {'groups': Group.objects.all().order_by('title')})
+        return render(request, 'students/students_add.html',
+                      {'groups': Group.objects.all().order_by('title')})
 
 
               # Якщо кнопка Скасувати була натиснута:
